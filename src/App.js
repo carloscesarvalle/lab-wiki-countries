234425b92ae4995391eca2f2c7cad167a9de7585
carloscesarvalle <carloscesarvalle@gmail.com>
2020-09-22 10:54:35 -0400

    adding and Api second solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from 'react';
+// import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Switch, Route } from 'react-router-dom'
-import Countries from './countries.json';
+
+//First solution importing a local json file
+// import Countries from './countries.json';
+
+//Second solution using an external API
+import axios from 'axios'
+
 import DisplayCountries from './components/DisplayCountries'
 import DisplayCountryDetails from './components/DisplayCountryDetails'
 import './App.css';
@@ -8,11 +15,28 @@ import './App.css';
 
 const App = () => {
 
+//Second solution: using hooks to fetch Api data and set state  
+const [Countries, setCountries] = useState([])
+
+useEffect(()=>{
+  const getData = async () => {
+    let res = await axios.get('https://countries.tech-savvy.tech/countries')
+    console.log(res)
+    setCountries(res.data)
+  }
+  getData()
+},[])
+
   return (
     <div className="App">
     <h1>Wiki Countries</h1>
+    {/* I'm passing the imported file Countries as a prop to DisplayCountries react child element */}
     <DisplayCountries countries={Countries}/>
     <Switch>
+    {/* I'm passing react-router-dom's props to the component DisplayCountryDetails. 
+    These are history, location and match. Inside match there is params, that has an ID. 
+    This ID is helpfull to match the element with other element with the same ID.  
+    You have to specify the path /:id that is chaging and taking each id as part of the url*/}
     <Route path='/:id' render={(props) => <DisplayCountryDetails {...props}/> }/>
     </Switch>
     </div>
--- src/components/DisplayCountryDetails.js
+++ src/components/DisplayCountryDetails.js
@@ -11,7 +11,10 @@ const DisplayCountryDetails = (props) => {
             <h1>Capital {matchCountry.name.official}</h1>
             <p>Area {matchCountry.area}</p>
             <p>Borders {matchCountry.borders.map((each,i) => {
-                return <Link to={`/${each}`}><li key={`key ${each[i]}`}>{each}</li></Link>})}</p>
+                return <Link to={`/${each}`}>
+                        <li key={`key ${each[i]}`}>{each}</li>
+                        </Link>})}
+            </p>
         </div>
     );
 };
